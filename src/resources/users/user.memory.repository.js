e4508d458790662ee879e6e6594b735f698972f6
Dmitry Uvarov <diman_niks@mail.ru>
2021-12-05 16:07:38 +0300

    feat: users crud

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,26 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+let db = [];
+
+const getAll = async () => db;
+
+const getOne = async (id) => {
+  const foundUser = db.find((user) => user.id === id);
+  return foundUser;
 };
 
-module.exports = { getAll };
+const create = async (user) => {
+  db.push(user);
+  return user;
+};
+
+const update = async (id, props) => {
+  const index = db.findIndex((p) => p.id === id);
+  db[index] = {id, ...props};
+  return db[index];
+};
+
+const remove = async (id) => {
+  db = db.filter((user) => user.id !== id);
+  
+};
+
+module.exports = { getAll, getOne, create, update, remove };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,38 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  res.status(200).json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const {id} = req.params;
+  const user = await usersService.getOne(id);
+  res.status(200).json(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+  const {body} = req;
+  const user = new User(body);
+  const newUser = await usersService.create(user);
+
+  res.status(201).json(User.toResponse(newUser));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const {id} = req.params;
+  const {body} = req;
+
+  const user = await usersService.update(id, body);
+
+  res.status(200).json(User.toResponse(user));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const {id} = req.params;
+  
+  await usersService.remove(id);
+
+  res.status(204).json();
 });
 
 module.exports = router;
