8b1fc68d498fdf1c18c88d1317765339d7baf9ac
Dmitry Uvarov <diman_niks@mail.ru>
2021-12-26 22:09:14 +0300

    fix: logger handlers

--- src/logger/handlers/errorHandler.ts
+++ src/logger/handlers/errorHandler.ts
@@ -3,20 +3,16 @@ import { errorLogger } from '../logger';
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.log('errorHandler');
     const { name, message, statusCode, stack } = err;
     const log = `${name} ${statusCode}: ${message}\n${stack}`;
 
     errorLogger.error(log);
 
-    const status = statusCode || 500;
-    res.status(status).send({ message });
-
-    errorLogger.on('finish', () => {
-        errorLogger.end();
-        console.log('never reaches here');
-        process.exit(1);
-    });
+    if (statusCode) {
+        res.status(statusCode).send({ message });
+    } else {
+        res.status(500).send({ message: 'Something wrong on server' });
+    }
 };
 
 export default errorHandler;
--- src/logger/handlers/requestHandler.ts
+++ src/logger/handlers/requestHandler.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { requestLogger } from '../logger';
 
 const requestHandler = (req: Request, res: Response, next: NextFunction) => {
-    console.log('requestHandler');
     const query = Object.keys(req.query).length > 0 ? JSON.stringify(req.query) : '';
     const body = Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : '';
     const log = `${req.method} ${req.url}\nQuery: ${query}\nBody: ${body}\n`;
